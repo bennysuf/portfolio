82a9c8f4036952282a806e7a54fb1dd11b38dccf
Benny Sufrin <bennysuf@gmail.com>
2023-09-20 10:49:37 -0700

    add project details to project list

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,10 +1,22 @@
 const projectList = [
     {
-        id: NaN,
-        name: "",
-        link: "",
-        video: "",
-    }
+        id: 1,
+        name: "Codify",
+        link: "https://github.com/bennysuf/Codify",
+        video: "https://youtu.be/0M-XUoCHVUk",
+    },
+    {
+        id: 2,
+        name: "Moms Cookbook",
+        link: "https://github.com/bennysuf/moms-cook-book",
+        video: "https://youtu.be/tjT1x5CfWpk?si=KRnfoo_9W2y0FX5i",
+    },
+    {
+        id: 3,
+        name: "Twidler",
+        link: "https://github.com/bennysuf/twidler-frontend",
+        video: "https://youtu.be/meK-mR5-XNU?si=F_bBvR6gJXeS3Av3",
+    },
 ]
 
 export default projectList;
