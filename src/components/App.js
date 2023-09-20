bb4fc6fb3f62c4aa024c609487b8689c43d75685
Benny Sufrin <bennysuf@gmail.com>
2023-09-20 12:41:26 -0700

    add Header component, add text align style

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import Contact from "./Contact";
 import NavBar from "./Nav";
 import ProjectCard from "./projects/ProjectCard";
 import ExperienceCard from "./experience/ExperienceCard";
+import Header from "./Header";
 import "@picocss/pico/css/pico.min.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{textAlign:"center"}}>
       <NavBar />
+      <Header />
       <ProjectCard />
       <ExperienceCard />
       <About />
@@ -18,3 +20,5 @@ function App() {
 }
 
 export default App;
+
+// TODO have fake drop down with a down or up arrow, when clicked, change state, ternary to see if arrow is up or down, display card or a bar with component title
