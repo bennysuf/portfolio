2ad409b68bfb1738be0f4070775356f6444eddd5
Benny Sufrin <bennysuf@gmail.com>
2023-09-20 12:40:46 -0700

    create component and add place holders

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <>
+        <h3>Benny Sufrin</h3> 
+        <h4>Full-stack Developer</h4>
+        {/* resume link and #connect */}
+        </>
+        );
+}
+
+export default Header;
