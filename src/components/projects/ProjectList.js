62b853878812fcb8582e8d0fd4d4132a30650641
Benny Sufrin <bennysuf@gmail.com>
2023-09-20 12:43:25 -0700

    add new keys and value

--- src/components/projects/ProjectList.js
+++ src/components/projects/ProjectList.js
@@ -1,22 +1,28 @@
 const projectList = [
-    {
-        id: 1,
-        name: "Codify",
-        link: "https://github.com/bennysuf/Codify",
-        video: "https://youtu.be/0M-XUoCHVUk",
-    },
-    {
-        id: 2,
-        name: "Moms Cookbook",
-        link: "https://github.com/bennysuf/moms-cook-book",
-        video: "https://youtu.be/tjT1x5CfWpk?si=KRnfoo_9W2y0FX5i",
-    },
-    {
-        id: 3,
-        name: "Twidler",
-        link: "https://github.com/bennysuf/twidler-frontend",
-        video: "https://youtu.be/meK-mR5-XNU?si=F_bBvR6gJXeS3Av3",
-    },
-]
+  {
+    id: 1,
+    name: "Codify",
+    description: "A platform facilitating hosting, discovery, and exploration of developer portfolios, projects, social links, and resumes.",
+    techStack: [],
+    github: "https://github.com/bennysuf/Codify",
+    video: "https://youtu.be/0M-XUoCHVUk",
+  },
+  {
+    id: 2,
+    name: "Moms Cookbook",
+    description: "An individualized recipe tracker app",
+    techStack: [],
+    github: "https://github.com/bennysuf/moms-cook-book",
+    video: "https://youtu.be/tjT1x5CfWpk?si=KRnfoo_9W2y0FX5i",
+  },
+  {
+    id: 3,
+    name: "Twidler",
+    description: "A peer-to-peer social media web app for thoughts and ideas",
+    techStack: [],
+    github: "https://github.com/bennysuf/twidler-frontend",
+    video: "https://youtu.be/meK-mR5-XNU?si=F_bBvR6gJXeS3Av3",
+  },
+];
 
 export default projectList;
