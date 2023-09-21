b0cb6a484b68e183a542ed55d7557cccd38bcd3f
Benny Sufrin <bennysuf@gmail.com>
2023-09-21 16:32:24 -0700

    add id/class names

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 function Header() {
     return (
-        <>
+        <body id="home">
         <h3>Benny Sufrin</h3> 
         <h4>Full-stack Developer</h4>
         {/* resume link and #connect */}
-        </>
+        </body>
         );
 }
 
