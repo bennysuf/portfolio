261a42d6cdc8900452f417305bdbdab2ad2c5cee
Benny Sufrin <bennysuf@gmail.com>
2023-09-21 16:33:17 -0700

    add tags and HashLinks

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,43 @@
+import { HashLink as Link } from "react-router-hash-link";
+
 function NavBar() {
-    return ( "" );
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link smooth to="#home" data-tooltip="Home" data-placement="bottom">
+            home
+            {/* TODO add icon here instead of word */}
+          </Link>
+        </li>
+        <li>
+          <Link
+            smooth
+            to="#project"
+            data-tooltip="Projects"
+            data-placement="bottom"
+          >
+            project
+          </Link>
+        </li>
+        <li>
+          <Link smooth to="#about" data-tooltip="About" data-placement="bottom">
+            about
+          </Link>
+        </li>
+        <li>
+          <Link
+            smooth
+            to="#contact"
+            data-tooltip="Contact"
+            data-placement="bottom"
+          >
+            contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default NavBar;
