742236e11e9e667279e3b66a1e8b01ec4825d2d2
Benny Sufrin <bennysuf@gmail.com>
2023-09-21 16:34:32 -0700

    add id/classnames and github/video links

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -2,18 +2,46 @@ import projectList from "./ProjectList";
 
 function ProjectCard() {
   return (
-    <body>
+    <body id="project">
       <h3>Projects</h3>
-      <article className="card-grid">
+      {/* <article className="card-grid"> */}
+      <article className="outer-article">
         <main class="container">
           <div class="grid">
             {projectList.map((project) => {
-              const { name, description, link, video } = project;
+              const { id, name, description, github, video, techStack } =
+                project;
+              const techSkills = techStack.map((skill) => (
+                <div className="skills">{skill}</div>
+              ));
               return (
-                <div>
-                  <article>
-                    <header>{name}</header>
+                <div key={id}>
+                  <article className="inner-article">
+                    <header className="article-header">{name}</header>
+                    <div className="skill-container">{techSkills}</div>
                     <p>{description}</p>
+                    <div className="portfolio_links">
+                      {github && (
+                        <a
+                          href={github}
+                          className="btn btn-primary"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          Github
+                        </a>
+                      )}
+                      {video && (
+                        <a
+                          href={video}
+                          className="btn btn-primary"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          Video
+                        </a>
+                      )}
+                    </div>
                   </article>
                 </div>
               );
