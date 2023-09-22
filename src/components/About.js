275e78e181935b7cbdbe75a2c3a2f52949ca8a6d
Benny Sufrin <bennysuf@gmail.com>
2023-09-22 11:14:47 -0700

    add tag and add About me paragraph

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,22 @@
 function About() {
   return (
-      <body id="about">
-        <h3>About Me</h3>
-        <article className="outer-article">
-          <main class="container">
-            <pre>hello itsa me, Benny</pre>
-          </main>
-        </article>
-      </body>
+    <body id="about">
+      <h3>About Me</h3>
+      <article className="outer-article">
+        <main class="container">
+          <article className="inner-article">
+          <p>
+            Passionate Full-Stack Developer with a creative flair, stemming from
+            a background in photography. Proficient in Photoshop and image
+            compositing. Adept at conceiving, refining, and troubleshooting
+            projects. IDF veteran with proven ability to excel under pressure,
+            collaborate in teams, and swiftly adapt. Quick learner with strong
+            hands-on skills, consistently delivering results.
+          </p>
+          </article>
+        </main>
+      </article>
+    </body>
   );
 }
 
