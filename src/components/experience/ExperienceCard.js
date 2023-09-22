90ade64794361ca65121392ad446943a6b2db34c
Benny Sufrin <bennysuf@gmail.com>
2023-09-22 11:16:39 -0700

    add classname and icons

--- src/components/experience/ExperienceCard.js
+++ src/components/experience/ExperienceCard.js
@@ -6,11 +6,12 @@ function ExperienceCard() {
       <h3>Technical Skills</h3>
       <article className="outer-article">
         <main class="container">
-          <article className="inner-article">
+          <article className="inner-article experience-container">
             {skillSet.map((skill) => {
+              const {id, name, icon, type, catagory} = skill
               return (
-                <div key={skill.id}>
-                  {skill.name}
+                <div key={id}>
+                {icon} {` ${name}`}
                   <br />
                 </div>
               );
